e3f82b7bfae536ad9ad6a2e3cf3aa4b0fee591e5
NoahMelle <noahmkamphuisen@gmail.com>
2024-12-04 10:54:33 +0100

    :zap: Improved performance of day 4 by removing unnecessary checks and using runes instead of strings

--- days/4/part1/main.go
+++ days/4/part1/main.go
@@ -4,10 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
+	"time"
 )
 
 func main() {
+	start := time.Now()
+
 	const filePath string = "./my_input_file.txt"
 
 	grid, err := parseFile(filePath)
@@ -18,29 +20,41 @@ func main() {
 
 	xmasCount := findXMAS(grid)
 	fmt.Println(xmasCount)
+
+	duration := time.Since(start)
+	fmt.Printf("\nExecuted the program in %s", duration)
 }
 
-func findXMAS(grid [][]string) int {
+func findXMAS(grid [][]rune) int {
 	const target = "XMAS"
+	const targetLen = len(target)
+
 	directions := []struct {
 		dx, dy int
 	}{
-		{1, 0},
-		{-1, 0},
-		{0, 1},
-		{0, -1},
-		{1, 1},
-		{-1, -1},
-		{1, -1},
-		{-1, 1},
+		{1, 0},   // rechts
+		{-1, 0},  // links
+		{0, 1},   // onder
+		{0, -1},  // boven
+		{1, 1},   // rechtsonder
+		{-1, -1}, // linksboven
+		{1, -1},  // rechtsboven
+		{-1, 1},  // linksonder
 	}
 
 	count := 0
 	for y, row := range grid {
 		for x := range row {
-			for _, dir := range directions {
-				if checkDirection(grid, x, y, dir.dx, dir.dy, target) {
-					count++
+			if row[x] == 'X' {
+				for _, dir := range directions {
+					// check out of bounds
+					if x+(targetLen-1)*dir.dx < 0 || x+(targetLen-1)*dir.dx >= len(row) ||
+						y+(targetLen-1)*dir.dy < 0 || y+(targetLen-1)*dir.dy >= len(grid) {
+						continue
+					}
+					if checkDirection(grid, x, y, dir.dx, dir.dy, target) {
+						count++
+					}
 				}
 			}
 		}
@@ -48,28 +62,28 @@ func findXMAS(grid [][]string) int {
 	return count
 }
 
-func checkDirection(grid [][]string, startX, startY, dx, dy int, target string) bool {
-	for i := 0; i < len(target); i++ {
+func checkDirection(grid [][]rune, startX, startY, dx, dy int, target string) bool {
+	for i := 1; i < len(target); i++ {
 		newX := startX + i*dx
 		newY := startY + i*dy
-		if newY < 0 || newY >= len(grid) || newX < 0 || newX >= len(grid[newY]) || grid[newY][newX] != string(target[i]) {
+		if grid[newY][newX] != rune(target[i]) {
 			return false
 		}
 	}
 	return true
 }
 
-func parseFile(path string) ([][]string, error) {
+func parseFile(path string) ([][]rune, error) {
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
 
-	var grid [][]string
+	var grid [][]rune
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		grid = append(grid, strings.Split(scanner.Text(), ""))
+		grid = append(grid, []rune(scanner.Text()))
 	}
 
 	return grid, scanner.Err()
--- days/4/part2/main.go
+++ days/4/part2/main.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"os"
 	"slices"
-	"strings"
+	"time"
 )
 
 func main() {
+	start := time.Now()
+
 	const filePath string = "./my_input_file.txt"
 
 	grid, err := parseFile(filePath)
@@ -18,17 +20,20 @@ func main() {
 	}
 
 	findMiddleLetter(grid)
+
+	duration := time.Since(start)
+	fmt.Printf("\nExecuted the program in %s", duration)
 }
 
-func findMiddleLetter(grid [][]string) {
+func findMiddleLetter(grid [][]rune) {
 	count := 0
 	for y := 1; y < len(grid)-1; y++ {
 		row := grid[y]
 		for x := 1; x < len(grid[y])-1; x++ {
-			if row[x] == "A" {
-				upperLeftToLowerRight := [2]string{grid[y-1][x-1], grid[y+1][x+1]}
-				upperRightToLowerLeft := [2]string{grid[y-1][x+1], grid[y+1][x-1]}
-				if slices.Contains(upperLeftToLowerRight[:], "M") && slices.Contains(upperLeftToLowerRight[:], "S") && slices.Contains(upperRightToLowerLeft[:], "M") && slices.Contains(upperRightToLowerLeft[:], "S") {
+			if row[x] == 'A' {
+				upperLeftToLowerRight := [2]rune{grid[y-1][x-1], grid[y+1][x+1]}
+				upperRightToLowerLeft := [2]rune{grid[y-1][x+1], grid[y+1][x-1]}
+				if slices.Contains(upperLeftToLowerRight[:], 'M') && slices.Contains(upperLeftToLowerRight[:], 'S') && slices.Contains(upperRightToLowerLeft[:], 'M') && slices.Contains(upperRightToLowerLeft[:], 'S') {
 					count++
 				}
 			}
@@ -38,17 +43,17 @@ func findMiddleLetter(grid [][]string) {
 	fmt.Println(count)
 }
 
-func parseFile(path string) ([][]string, error) {
+func parseFile(path string) ([][]rune, error) {
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
 
-	var grid [][]string
+	var grid [][]rune
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		grid = append(grid, strings.Split(scanner.Text(), ""))
+		grid = append(grid, []rune(scanner.Text()))
 	}
 
 	return grid, scanner.Err()
